71861eb51ca37494fd18c5220203a3cebd47bf94
Dale O Shea <daleoshea@gmail.com>
2018-02-17 19:52:00 +0000

    added create user function

--- django/mydivesapi/mydives_api/models.py
+++ django/mydivesapi/mydives_api/models.py
@@ -3,21 +3,39 @@ from django.contrib.auth.models import AbstrachBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+class UserProfile(AbstrachBaseUser, PermissionsMixin):
 
-name = models.CharField(max_length = 250);
-email = models.CharField(max_length = 250, unique=True);
-is_staff = models.BooleanField(default = False);
+name = models.CharField(max_length = 250)
+email = models.CharField(max_length = 250, unique=True)
+is_staff = models.BooleanField(default = False)
 is_active = models.BooleanField(default = True)
 
-USERNAME_FIELD = 'email';
-REQUIRED_FIELDS = ['name'];
+objects = UserManager()
+
+USERNAME_FIELD = 'email'
+REQUIRED_FIELDS = ['name']
 
 
 def get_full_name(self):
     return self.email
 
-def get_short_name
+def get_short_name(self):
     return self.name
 
 def __str__(self):
     return self.email
+
+class UserManager(BaseUserManager):
+    def create_user(self, name, email password = None):
+
+        if not email:
+            raise ValueError('Please enter an email address')
+
+        user = self.model(
+               email = self.normalize_email(email),
+               name = name
+               )
+
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
