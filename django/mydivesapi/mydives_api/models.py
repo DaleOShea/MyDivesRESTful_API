63aa4966db208db6e6ee6c1d07fa592a46fc3a23
Dale O Shea <daleoshea@gmail.com>
2018-02-17 19:37:27 +0000

    user attributes fields

--- django/mydivesapi/mydives_api/models.py
+++ django/mydivesapi/mydives_api/models.py
@@ -4,4 +4,20 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
-# Create your models here.
+name = models.CharField(max_length = 250);
+email = models.CharField(max_length = 250, unique=True);
+is_staff = models.BooleanField(default = False);
+is_active = models.BooleanField(default = True)
+
+USERNAME_FIELD = 'email';
+REQUIRED_FIELDS = ['name'];
+
+
+def get_full_name(self):
+    return self.email
+
+def get_short_name
+    return self.name
+
+def __str__(self):
+    return self.email
