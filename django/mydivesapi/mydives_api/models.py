8f383879e693330a3de8e870d1cc34b615c0b882
Dale O Shea <daleoshea@gmail.com>
2018-02-17 20:18:43 +0000

    refractoring of models class

--- django/mydivesapi/mydives_api/models.py
+++ django/mydivesapi/mydives_api/models.py
@@ -3,26 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-class UserProfile(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(max_length = 250)
-    email = models.CharField(max_length = 250, unique=True)
-    is_staff = models.BooleanField(default = False)
-    is_active = models.BooleanField(default = True)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
-
-
-    def get_full_name(self):
-        return self.email
-
-    def get_short_name(self):
-        return self.name
-
-    def __str__(self):
-        return self.email
 
 class UserManager(BaseUserManager):
     def create_user(self, name, email, password = None):
@@ -47,3 +27,24 @@ class UserManager(BaseUserManager):
         user.save(using = self._db)
 
         return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length = 250)
+    email = models.CharField(max_length = 250, unique=True)
+    is_staff = models.BooleanField(default = False)
+    is_active = models.BooleanField(default = True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
