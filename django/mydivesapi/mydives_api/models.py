918dcb00d85505b479fb2f555b1e14e6278e2722
Dale O Shea <daleoshea@gmail.com>
2018-02-17 20:16:35 +0000

    fixed indentation errors and spelling mistake in import

--- django/mydivesapi/mydives_api/models.py
+++ django/mydivesapi/mydives_api/models.py
@@ -1,32 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import AbstrachBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-class UserProfile(AbstrachBaseUser, PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length = 250)
+    email = models.CharField(max_length = 250, unique=True)
+    is_staff = models.BooleanField(default = False)
+    is_active = models.BooleanField(default = True)
 
-name = models.CharField(max_length = 250)
-email = models.CharField(max_length = 250, unique=True)
-is_staff = models.BooleanField(default = False)
-is_active = models.BooleanField(default = True)
+    objects = UserManager()
 
-objects = UserManager()
-
-USERNAME_FIELD = 'email'
-REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
 
-def get_full_name(self):
-    return self.email
+    def get_full_name(self):
+        return self.email
 
-def get_short_name(self):
-    return self.name
+    def get_short_name(self):
+        return self.name
 
-def __str__(self):
-    return self.email
+    def __str__(self):
+        return self.email
 
 class UserManager(BaseUserManager):
-
     def create_user(self, name, email, password = None):
 
         if not email:
@@ -41,14 +39,11 @@ class UserManager(BaseUserManager):
         user.save(using = self._db)
         return user
 
-def create_superuser(self, name, email, password):
+    def create_superuser(self, name, email, password):
 
-    user = self.create_user(
-                name,
-                email,
-                password = password
-                )
+        user = self.create_user(name, email, password = password)
 
-                user.is_admin = True
-                user.save(using = self._db)
-    return user 
+        user.is_admin = True
+        user.save(using = self._db)
+
+        return user
