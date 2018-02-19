65160552563b140a3add913ac934f3226a4358bc
Dale O Shea <daleoshea@gmail.com>
2018-02-19 19:08:27 +0000

    fixed error in serializer class and added sample response object for testing

--- django/mydivesapi/mydives_api/views.py
+++ django/mydivesapi/mydives_api/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from . import serializers
+from rest_framework import status
 # Create your views here.
 
 class ListLocations(APIView):
 
+    serializer = serializers.LocationSerializer
+
     def get(self, request, format = None):
 
+        sampleDictionaryLocation = [
+            'One',
+            'Two',
+            'Three',
+            'Four',
+            'Five',
+        ]
 
-        return Response()
+        return Response({'Location' : sampleDictionaryLocation})
