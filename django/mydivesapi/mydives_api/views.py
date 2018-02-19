5c7bd08a278fa4414ba2c1d1a168558fbed7265e
Dale O Shea <daleoshea@gmail.com>
2018-02-19 18:31:57 +0000

    created urls file

--- django/mydivesapi/mydives_api/views.py
+++ django/mydivesapi/mydives_api/views.py
@@ -7,3 +7,6 @@ from rest_framework.response import Response
 class ListLocations(APIView):
 
     def get(self, request, format = None):
+
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
