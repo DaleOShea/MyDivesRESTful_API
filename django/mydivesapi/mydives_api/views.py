9d917920d968ef9528ddc33b8062fd0ac76994d1
Dale O Shea <daleoshea@gmail.com>
2018-02-19 18:53:01 +0000

    created a serializer class

--- django/mydivesapi/mydives_api/views.py
+++ django/mydivesapi/mydives_api/views.py
@@ -8,5 +8,5 @@ class ListLocations(APIView):
 
     def get(self, request, format = None):
 
-        usernames = [user.username for user in User.objects.all()]
-        return Response(usernames)
+
+        return Response()
