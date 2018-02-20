60f53e909d14c7fb1d152ea42eb72f0e799a1636
Dale O Shea <daleoshea@gmail.com>
2018-02-20 15:32:08 +0000

    filtering set up changes, app crashing at this point

--- django/mydivesapi/mydives_api/views.py
+++ django/mydivesapi/mydives_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from . import models
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class ListLocations(APIView):
--- django/mydivesapi/mydivesapi/settings.py
+++ django/mydivesapi/mydivesapi/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'mydives_api.apps.MydivesApiConfig',
-    
+    'django_filters',
+
 ]
 
 MIDDLEWARE = [
