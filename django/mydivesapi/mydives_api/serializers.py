d0450480f11ac45927ddcb3bf1f78ba966c1a6c9
Dale O Shea <daleoshea@gmail.com>
2018-02-20 14:30:31 +0000

    loc info serializer

--- django/mydivesapi/mydives_api/serializers.py
+++ django/mydivesapi/mydives_api/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
+from . import models
 
 
 class LocationSerializer(serializers.Serializer):
     diveLocation = serializers.CharField(min_length = 5, max_length = 70, allow_blank = False)
+
+
+class LocationInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.LocationInfo
+        fields = ('id', 'LocType', 'location', 'latitude', 'longitude', 'details')
